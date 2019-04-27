cda8896d1e91d6daee53561db1f8e0d47ac4aa1a
kana4477 <kana_demoto@demotokana-no-MacBook-Pro.local>
2019-04-27 16:28:56 +0900

    edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,31 +1,40 @@
 $(function(){
-  // function buildHTML(message){
-  //   var html = `<p>
-  //                 <strong>
-  //                   <a href=groups/${message.group_id}/messages></a>
-  //                   :
-  //                 </strong>
-  //                   ${message.message}
-  //               </p>`
+  function buildHTML(message){
+    var html = ` <div class="chatArea__chat">
+                    <div class="chatArea__chat--name">
+                          ${user_id}
+                    </div>
+                    <div class="chatArea__chat--date">
+                          ${created_at("%Y/%m/%d %H:%M")}
+                    <div class="chatArea__chat--message">
+                          ${message}
+                    </div>
+                  </div>
+                ` 
+    return html;
+  }
 
-  //  }
-  $('#sendMessageBtn').submit(function(e){
-    e.preventDefalt();
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
     var formData = new FormData(this);
+    var href = window.location.href
     $.ajax({
-      url: 'messages/create',
+      url: href,
       type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false
+      contentType: false,
     })
-    .done(function(data){ // var html = buildHTML(data);
-      
-    }) 
+    .done(function(data){ 
+      // alert('sucsess!')
+      var html = buildHTML(data);
+      $('.chatArea').append(html)
+      $('#sendMessageText').val()
+     }) 
 
     .fail(function(){
-      alert('error')
+      alert('error!')
     })
 
   });
